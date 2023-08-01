2c9ab3662add3cae36d14b42cc7f9b919ce75d0c
jihwan98 <jihwan98.dev@gmail.com>
2023-08-01 10:43:59 +0900

    메인페이지 접근 시 로그인 하도록

--- src/main/java/com/mysite/checkin/controller/MainController.java
+++ src/main/java/com/mysite/checkin/controller/MainController.java
@@ -1,11 +1,12 @@
 package com.mysite.checkin.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/main")
     public String main() {
         return "main";
